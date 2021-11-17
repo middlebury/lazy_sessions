8d605bbef9ed482d5a75797c1d486ef6671d8dd8
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-17 10:36:06 +0800

    Delete example directory

--- example/docroot/no_data_set_1.php
+++
@@ -1,5 +0,0 @@
-<?php
-require('../library/bootstrap.php');
-
-$title = 'No-data-set 1';
-include('../library/content.php');
--- example/docroot/no_data_set_2.php
+++
@@ -1,5 +0,0 @@
-<?php
-require('../library/bootstrap.php');
-
-$title = 'No-data-set 2';
-include('../library/content.php');
